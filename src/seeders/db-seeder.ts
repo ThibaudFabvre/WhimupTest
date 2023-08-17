85fec3007238d3073d7b2380a53cc1a2417eeccd
Thibaud Fabvre <thibaudfabvre@thibauds-macbook-pro.home>
2023-08-17 13:59:21 +0200

    add example to seeder for date comparaison

--- src/seeders/db-seeder.ts
+++ src/seeders/db-seeder.ts
@@ -1,4 +1,3 @@
-
 import client from '../database';
 
 // export const fakeMatches = [
@@ -63,15 +62,18 @@ const seedDB = () => {
 
             INSERT INTO "public".matches (id, status, location, date, game, results)
             VALUES
+            (67, 'upcoming', 'Paris', 1755428873000, 'football', array['{ "id": 1, "result": 0 }','{ "id": 3, "result": 1 }']::json[])
             (42, 'ongoing', 'Toulouse', 1692110735317, 'football', array['{ "id": 1, "result": 0 }', '{ "id": 2, "result": 1 }']::json[]),
             (36, 'finished', 'Toulouse', 1692110735317, 'football', array['{ "id": 1, "result": 0 }','{ "id": 2, "result": 1 }']::json[]),
-            (75, 'upcoming', 'Paris', 1849792030000, 'football', array['{ "id": 1, "result": 0 }','{ "id": 3, "result": 1 }']::json[]);
+            (75, 'upcoming', 'Paris', 1849792030000, 'football', array['{ "id": 1, "result": 0 }','{ "id": 3, "result": 1 }']::json[])
+            ;
+            
 
             INSERT INTO "public"."teams" (id, name, logo, matches)
             VALUES 
             (1, 'Toulouse', 'https://upload.wikimedia.org/wikipedia/fr/thumb/5/58/Toulouse_FC_logo_2021.svg/1200px-Toulouse_FC_logo_2021.svg.png', ARRAY [42, 75]),
             (2, 'Marseilles', 'https://www.1min30.com/wp-content/uploads/2018/03/Couleur-logo-Olympique-de-Marseille.jpg', ARRAY [42]),
-            (3, 'Paris', 'https://upload.wikimedia.org/wikipedia/fr/thumb/8/86/Paris_Saint-Germain_Logo.svg/600px-Paris_Saint-Germain_Logo.svg.png', ARRAY [75])
+            (3, 'Paris', 'https://upload.wikimedia.org/wikipedia/fr/thumb/8/86/Paris_Saint-Germain_Logo.svg/600px-Paris_Saint-Germain_Logo.svg.png', ARRAY [75]),
         `;
 
   try {
@@ -81,4 +83,4 @@ const seedDB = () => {
   }
 };
 
-//seedDB()
+//seedDB();
